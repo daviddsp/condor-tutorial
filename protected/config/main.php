38dcabc2cfd5b44d7407c2521c43f30e244b505f
David Solórzano <abdsp22@gmail.com>
2013-06-04 10:31:35 -0430

    Se coloco el correo electronico de contacto
    Se agrego el correo en la vista del index y en la configuración de yii para volverlo un parametro mas del proyecto la cual se puede llamar utilizando 'adminEmail'

--- protected/config/main.php
+++ protected/config/main.php
@@ -101,7 +101,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'pcondor@gmail.com',
 	),
 	
 	'language'=>'es',
